27e66bfa2e2b0c8f3a6c2f498aa6f7a6c937314e
durga <durgaprasad230698@gmail.com>
2023-05-22 12:05:38 +0530

    Changes in nested components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const DurgaPrasad = () => {
+  return <h1>My name is Durga Prasad</h1>
+};
+// eslint-disable-next-line
+const PrintName = () => {
+  return <DurgaPrasad />;
+};
+
+const App = () => {
+  // eslint-disable-next-line
+ /*const user = {
+    firstName: "Durga",
+    lastName: "Prasad", 
+  };*/
+return (
+  <div>
+    <PrintName />
+    <PrintName />
+    <PrintName />
+    <PrintName />
+    <PrintName />
+    <PrintName />
+  </div>
+);
+};
 
 export default App;
